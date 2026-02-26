91f0a68998a161411522b4562c12b944c0a07161
Ashikmahbub <ashikmahbub56@gmail.com>
2026-02-26 10:29:48 +0600

    added celery

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -60,6 +60,8 @@ INSTALLED_APPS = [
      
     'users',
     'corsheaders',
+    'django_celery_beat',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -177,3 +179,9 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 CORS_ALLOW_ALL_ORIGINS = True
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://redis:6379/1')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://redis:6379/1')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
