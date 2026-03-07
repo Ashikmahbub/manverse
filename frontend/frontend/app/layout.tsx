f84b8ff209243930c279cc87a238c5eb658a0d72
Ashikmahbub <ashikmahbub56@gmail.com>
2026-03-07 23:22:11 +0600

    layoutfixed

--- frontend/frontend/app/layout.tsx
+++ frontend/frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { CartProvider } from "../store/cartStore";
+import { CartProvider } from "../app/store/cartStore";
 import ChatBox from "./components/ChatBox";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
