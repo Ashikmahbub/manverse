eaa0d3df0a353ce57343be341e43f5fbcce53ffb
Ashikmahbub <ashikmahbub56@gmail.com>
2026-03-07 13:56:25 +0600

    card fix

--- frontend/frontend/app/layout.tsx
+++ frontend/frontend/app/layout.tsx
@@ -1,40 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
-import Providers from "./provider/Provider";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+ 
+import { CartProvider } from "../app/store/cartStore";
 import ChatBox from "./components/ChatBox";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MANVERSE | FASHION",
   description: "Premium athletic fashion for modern confidence.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>
-          <Navbar />
+        <CartProvider>
           {children}
-          <Footer />
           <ChatBox />
-        </Providers>
+        </CartProvider>
       </body>
     </html>
   );
