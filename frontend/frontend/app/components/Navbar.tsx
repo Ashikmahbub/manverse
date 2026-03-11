448e377665fb87209c3f1121d243eaf3178499da
Ashikmahbub <ashikmahbub56@gmail.com>
2026-03-11 13:49:27 +0600

    navbar directory fixed

--- frontend/frontend/app/components/Navbar.tsx
+++ frontend/frontend/app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { useCart } from "../../store/cartStore";
+import { useCart } from "../store/cartStore";
 import { useRouter } from "next/navigation";
 
 export default function Navbar() {
