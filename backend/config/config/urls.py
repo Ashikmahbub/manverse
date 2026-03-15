1109d7a0c30b189879d75107bafbfb666fc9645d
Ashikmahbub <ashikmahbub56@gmail.com>
2026-03-15 11:58:32 +0600

    urls fixed

--- backend/config/config/urls.py
+++ backend/config/config/urls.py
@@ -2,16 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/",        admin.site.urls),
     path("api/products/", include("products.urls")),
-    path("api/orders/", include("orders.urls")),
-    path("api/users/", include("users.urls")),
+    path("api/orders/",   include("orders.urls")),
+    path("api/users/",    include("users.urls")),
 ]
-
-
-# Now your URLs will be:
-# ```
-# POST /api/users/register/       ← register
-# POST /api/users/login/          ← get tokens
-# POST /api/users/token/refresh/  ← refresh token
-# GET  /api/users/profile/        ← user profile
